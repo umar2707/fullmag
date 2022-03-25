d3c52c1bf9b7abebf655301a4ff294b053933d22
umar2707 <nurbekatajonov67@gmail.com>
2022-03-25 16:33:26 +0500

    server cala joyi boldi

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ const authRoute = require("./routes/auth")
 const productRoute = require("./routes/product")
 const cartRoute = require("./routes/cart")
 const orderRoute = require("./routes/order")
+const cors = require("cors");
 dotenv.config()
 mongoose
 .connect(process.env.MONGO_URL)
 .then(()=>{console.log('DB Connection  Succesfull!')})
 .catch((err)=>{console.log(err)})
-
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+}
+app.use(cors(corsOptions)) // Use this after the variable declaration
 app.use(express.json())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)

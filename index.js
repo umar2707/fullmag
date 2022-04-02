07ce68e94fa535edb1521e8091be7626107e0b75
umar2707 <nurbekatajonov67@gmail.com>
2022-04-02 22:22:29 +0500

    a

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const productRoute = require("./routes/product")
 const cartRoute = require("./routes/cart")
 const orderRoute = require("./routes/order")
 const cors = require("cors");
+const path = require('path')
 dotenv.config()
 mongoose
 .connect(process.env.MONGO_URL)
@@ -21,8 +22,12 @@ app.use("/api/users",userRoute)
 app.use("/api/products",productRoute)
 app.use("/api/carts",cartRoute)
 app.use("/api/orders",orderRoute)
+app.use(express.static(path.join(__dirname,"../client/build/index.html")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"../client/build/index.html"))
+})
 
 app.listen(process.env.PORT || 5000,()=>{
-    console.log("Backend server is running");
+    console.log("Backend server is running ", process.env.PORT || 5000);
 })
 
